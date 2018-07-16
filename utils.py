2093fac545e2c459b1cd6d6bf22abaed54fc110c
Raghav Goyal <raghavgoyal14@gmail.com>
2018-07-16 14:01:04 +0200

    added  removing func

---
+++ utils.py
@@ -0,0 +1,11 @@
+def remove_module_from_checkpoint_state_dict(state_dict):
+    """
+    Removes the prefix `module` from weight names that gets added by
+    torch.nn.DataParallel()
+    """
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:]  # remove `module.`
+        new_state_dict[name] = v
+    return new_state_dict
